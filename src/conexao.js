6974654f4c0a6af29f3998e028dbbe2c2aed5944
taffarel55 <mauruciotaffarel@gmail.com>
2021-10-25 23:22:08 -0300

    add banco

---
+++ src/conexao.js
@@ -0,0 +1,11 @@
+const knex = require("knex")({
+  client: "pg",
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
+});
+
+module.exports = knex;
