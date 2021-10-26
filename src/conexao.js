0ffe319566e18a23602a110348e9a258534d749a
taffarel55 <mauruciotaffarel@gmail.com>
2021-10-25 23:37:38 -0300

    true

--- src/conexao.js
+++ src/conexao.js
@@ -1,10 +1,10 @@
 const knex = require("knex")({
   client: "pg",
   connection: {
-    connectionString:
-      process.env.DATABASE_URL ||
-      "postgresql://postgres:postgres@localhost:5432/mini_insta",
-    ssl: process.env.DATABASE_URL ? true : false,
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: true,
+    },
   },
 });
 
